e2fe28d889203c1ad251dd27f14821f8580bcfcc
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-10 00:07:59 -0300

    chore: remove unused import

--- src/app/services/getManga.service.ts
+++ src/app/services/getManga.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { typeCard } from '../card/card.component';
 
