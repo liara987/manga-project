fc0d1dd9148c7fcc406e467b205951980184f31b
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-10 03:53:18 -0300

    update: create service to get manga by title

--- src/app/services/getManga.service.ts
+++ src/app/services/getManga.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { typeCard } from '../card/card.component';
 
@@ -16,5 +16,15 @@ export class GetMangaService {
 
     public getCoverFileName(id_cover_art: string) {
         return this.http.get(`${BASE_URL}/cover/${id_cover_art}`)
+    }
+
+    public getMangaByTitle(title: string) {
+        const options = { params: new HttpParams().set('title', title) };
+        return this.http.get(`${BASE_URL}/manga/`, options)
+    }
+
+    public getMangaCover(id_cover_art: string, file_name: string) {
+        const BASE_IMAGE_URL = 'https://uploads.mangadex.org'
+        return (`${BASE_IMAGE_URL}/covers/${id_cover_art}/${file_name}.256.jpg`)
     }
 }
