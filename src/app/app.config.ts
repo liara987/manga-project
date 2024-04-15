61e5989244e54a34c8e3f00c74dbf45e47cd3264
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-15 17:09:03 -0300

    fix: fix page not found on reload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { GetMangaService } from './services/getManga.service';
 
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     GetMangaService
   ]
 };
