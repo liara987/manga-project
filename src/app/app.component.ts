562709e5e7eeb38ea177bdb993de6ccde9311979
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-17 20:17:29 -0300

    chore: import navbar and getMangaService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,23 @@ import { CarouselModule } from '@coreui/angular';
 import { HttpClientModule } from '@angular/common/http';
 import { GetMangaService } from './services/getManga.service';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [
-    RouterOutlet,
-    HttpClientModule,
-    CarouselModule,
-    HomeComponent,
-    RouterLink, 
-    RouterLinkActive
-  ],
-  providers: [GetMangaService]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    providers: [GetMangaService],
+    imports: [
+        RouterOutlet,
+        HttpClientModule,
+        CarouselModule,
+        HomeComponent,
+        RouterLink,
+        RouterLinkActive,
+        NavbarComponent
+    ]
 })
 export class AppComponent {
   title = 'manga-project';
