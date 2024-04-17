40250effe36888e32c093e894e8148c98d8615d8
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-17 20:58:28 -0300

    feature: add home icon from coreui in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IconDirective } from '@coreui/icons-angular';
+import { cilHome } from '@coreui/icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, IconDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  icons = { cilHome };
 }
