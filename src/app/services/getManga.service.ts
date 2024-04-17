a92559b0b46ea8086c0bbc7709443c491e4780ac
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-17 20:18:32 -0300

    feature: create getMangaChapterList service and set Observable as return type from functions

--- src/app/services/getManga.service.ts
+++ src/app/services/getManga.service.ts
@@ -1,30 +1,49 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { typeCard } from '../card/card.component';
+import { Observable } from 'rxjs';
 
 const BASE_URL = 'https://api.mangadex.org'
+const BASE_IMAGE_URL = 'https://uploads.mangadex.org'
 @Injectable({ providedIn: 'root' })
 export class GetMangaService {
     public getDataJson: any;
-    mangaInfo: Array<typeCard> = [];
+    public languages = ['en'];
+    public options = {};
 
     constructor(private http: HttpClient) { }
 
-    public getAllMangas() {
-        return this.http.get(`${BASE_URL}/manga?limit=20`)
+    public getAllMangas(): Observable<any> {
+        this.options = {
+            params: {
+                'availableTranslatedLanguage[]': 'en',
+                'limit': '100',
+            }
+        };
+        return this.http.get(`${BASE_URL}/manga`, this.options)
     }
 
-    public getCoverFileName(id_cover_art: string) {
+    public getCoverFileName(id_cover_art: string): Observable<any> {
         return this.http.get(`${BASE_URL}/cover/${id_cover_art}`)
     }
 
-    public getMangaByTitle(title: string) {
+    public getMangaByTitle(title: string): Observable<any> {
         const options = { params: new HttpParams().set('title', title) };
         return this.http.get(`${BASE_URL}/manga/`, options)
     }
 
-    public getMangaCover(id_cover_art: string, file_name: string) {
-        const BASE_IMAGE_URL = 'https://uploads.mangadex.org'
+    public getMangaCover(id_cover_art: string, file_name: string): string {
         return (`${BASE_IMAGE_URL}/covers/${id_cover_art}/${file_name}.256.jpg`)
+    }
+
+    public getMangaChapterList(id_manga: string): Observable<any> {
+        this.options = {
+            params: {
+                'translatedLanguage[]': this.languages,
+                'order[createdAt]': 'asc',
+            }
+        };
+
+        return this.http.get(`${BASE_URL}/manga/${id_manga}/feed`, this.options)
     }
 }
