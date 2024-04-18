c3833c8f631195085e3631ab5ab272e7b6a29fc4
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-18 00:33:31 -0300

    chore: add readchapter router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MangaDetailComponent } from './manga-detail/manga-detail.component';
 import { HomeComponent } from './home/home.component';
+import { ReadChapterComponent } from './read-chapter/read-chapter.component';
 
 export const routes: Routes = [
     {path: 'manga/:title/:image', component: MangaDetailComponent},
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
+    {path: 'chapter/:id_chapter', component: ReadChapterComponent},
 ];
