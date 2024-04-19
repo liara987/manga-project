cfaecb73ad23181b67e967d3275e39f926cde6d5
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-19 20:42:50 -0300

    feature: add pagination to manga

--- src/app/manga-detail/manga-detail.component.ts
+++ src/app/manga-detail/manga-detail.component.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { GetMangaService } from '../services/getManga.service';
 import { ImgModule } from '@coreui/angular';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterLink } from '@angular/router';
+import { PageItemDirective, PageLinkDirective, PaginationComponent } from '@coreui/angular';
 
 export interface typeDetailManga {
   id: string,
@@ -23,12 +24,21 @@ export interface typeDetailManga {
   standalone: true,
   templateUrl: './manga-detail.component.html',
   styleUrl: './manga-detail.component.scss',
-  imports: [ImgModule, CommonModule, RouterModule]
+  imports: [
+    ImgModule,
+    CommonModule,
+    RouterModule,
+    PaginationComponent,
+    PageItemDirective,
+    PageLinkDirective,
+    RouterLink
+  ]
 })
 export class MangaDetailComponent {
   mangaDetail = <typeDetailManga>{};
   genreList: string[] = []
   chapterList: any[] = []
+  page = 1
 
   constructor(private route: ActivatedRoute, private mangaService: GetMangaService) { }
 
@@ -39,19 +49,20 @@ export class MangaDetailComponent {
     this.mangaDetail.title = mangaRouterTitle
     this.mangaDetail.image = mangaRouterImage
 
-    this.setMangaByTitle(mangaRouterTitle)    
+    this.setMangaByTitle(mangaRouterTitle)
   }
 
   getMangaChapterList(id_manga: string) {
-    this.mangaService.getMangaChapterList(id_manga).subscribe((mangaDetailData: any) => {
-      this.chapterList = mangaDetailData.data;      
+    this.mangaService.getMangaChapterList(id_manga, this.page).subscribe((mangaDetailData: any) => {
+      this.chapterList = mangaDetailData.data;
     })
   }
-  
+
   setMangaByTitle(mangaRouterTitle: string) {
     this.mangaService.getMangaByTitle(mangaRouterTitle).subscribe((mangaDetailData: any) => {
       this.mangaDetail.status = mangaDetailData.data[0].attributes.status
       this.mangaDetail.id = mangaDetailData.data[0].id
+
       this.mangaDetail.description = mangaDetailData.data[0].attributes.description.en
       this.mangaDetail.type = mangaDetailData.data[0].type
       this.mangaDetail.yearLauch = mangaDetailData.data[0].attributes.year
@@ -63,5 +74,20 @@ export class MangaDetailComponent {
 
       this.getMangaChapterList(this.mangaDetail.id)
     });
+  }
+
+  nextPage() {
+    this.page += 50
+    this.getMangaChapterList(this.mangaDetail.id)
+  }
+
+  setPage(pageNumber: number) {
+    this.page = pageNumber
+    this.getMangaChapterList(this.mangaDetail.id)
+  }
+
+  previousPage() {
+    this.page -= 50
+    this.getMangaChapterList(this.mangaDetail.id)
   }
 }
