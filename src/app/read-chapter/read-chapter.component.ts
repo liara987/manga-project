aff648e56155f4d5a2582140ba8487ccc5a21da1
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-19 15:57:33 -0300

    chore: remove go-to-top code from read-chapter

--- src/app/read-chapter/read-chapter.component.ts
+++ src/app/read-chapter/read-chapter.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { GetMangaService } from '../services/getManga.service';
 import { ActivatedRoute } from '@angular/router';
-import { IconDirective } from '@coreui/icons-angular';
-import { cilArrowThickTop } from '@coreui/icons';
-import { ImgModule } from '@coreui/angular';
+import { GoToTopComponent } from "../go-to-top/go-to-top.component";
 
 @Component({
   selector: 'app-read-chapter',
   standalone: true,
-  imports: [IconDirective, ImgModule],
   templateUrl: './read-chapter.component.html',
-  styleUrl: './read-chapter.component.scss'
+  styleUrl: './read-chapter.component.scss',
+  imports: [GoToTopComponent]
 })
 export class ReadChapterComponent {
-  icons = { cilArrowThickTop };
   imageList = [];
   hash!: string;
   images: string[] = [];
@@ -30,19 +27,18 @@ export class ReadChapterComponent {
         console.log(error);
       },
       complete: () => {
-        console.log('Compleet');
         this.imageList.forEach((x, i) => {
           this.images[i] = this.mangaService.getChapterImage(this.hash, x)
         });
       }
     })
   }
-  //cil-arrow-thick-top
-  onClickGoToTop(){
-    window.scroll({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'smooth' 
-});
+
+  onClickGoToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
