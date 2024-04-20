9ce7066aee0db88c48a5bf16525c7e5b97b14ea4
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-20 02:10:25 -0300

    fix: dont try to show more manga than it has and create showmore funcion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,20 +7,25 @@ import { CardComponent } from "../card/card.component";
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SpinnerModule } from '@coreui/angular';
+import { IconDirective } from '@coreui/icons-angular';
+import { cilChevronCircleDownAlt } from '@coreui/icons';
+import { GoToTopComponent } from "../go-to-top/go-to-top.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [
-    NavbarComponent,
-    CarouselComponent,
-    CardComponent,
-    CommonModule,
-    RouterModule,
-    SpinnerModule
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [
+        IconDirective,
+        NavbarComponent,
+        CarouselComponent,
+        CardComponent,
+        CommonModule,
+        RouterModule,
+        SpinnerModule,
+        GoToTopComponent
+    ]
 })
 export class HomeComponent {
   cardContent: Array<typeCard> = [];
@@ -30,13 +35,22 @@ export class HomeComponent {
   image!: string;
   mangaID!: string;
   showMangaList = false;
+  page = 1
+  icons = { cilChevronCircleDownAlt };
 
   constructor(private mangaService: GetMangaService) { }
 
   ngOnInit(): void {
-    this.mangaService.getAllMangas().subscribe({
+    this.getManga()
+  }
+
+  getManga() {
+    this.mangaService.getAllMangas(this.page).subscribe({
       next: (mangaData: any) => {
         mangaData.data.forEach((mangaItem: any) => {
+          if (mangaData.data.length < 96) {
+            this.page = mangaData.data.length
+          }
           this.coverId = this.mangaService.getCoverId(mangaItem)
           this.mangaService.getCoverFileName(this.coverId).subscribe((cover: any) => {
             this.fileName = cover.data.attributes.fileName
@@ -54,6 +68,11 @@ export class HomeComponent {
       },
       complete: () => { this.showMangaList = true }
     });
+  }
+
+  showMore() {
+    this.page += 96
+    this.getManga()
   }
 
   setCardContent(id: string, image: string, title: string) {
