9a28eab9b54aae2935701a9cf95d9c46c4d1e50e
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-04-20 02:08:33 -0300

    fix: create function to go to top

--- src/app/go-to-top/go-to-top.component.ts
+++ src/app/go-to-top/go-to-top.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { cilArrowThickTop } from '@coreui/icons';
-import { ImgModule } from '@coreui/angular';
+import { ImgModule, ButtonDirective } from '@coreui/angular';
 import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-go-to-top',
   standalone: true,
-  imports: [IconDirective, ImgModule,],
+  imports: [IconDirective, ImgModule, ButtonDirective,],
   templateUrl: './go-to-top.component.html',
   styleUrl: './go-to-top.component.scss'
 })
 export class GoToTopComponent {
   icons = { cilArrowThickTop };
+
+  onClickGoToTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
 }
