b65caf59a5c2fd27b79b7672f3cbf81b65efc073
liara987 <987.liara@gmail.com>
2025-08-11 21:16:14 -0300

    fix: tenta corrigir cors

--- src/app/services/getManga.service.ts
+++ src/app/services/getManga.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
-const BASE_URL = 'https://api.mangadex.org';
+const BASE_URL = '/api';
 const BASE_IMAGE_URL = 'https://uploads.mangadex.org';
 @Injectable({ providedIn: 'root' })
 export class GetMangaService {
@@ -22,20 +21,20 @@ export class GetMangaService {
         'contentRating[]': ['suggestive', 'safe'],
       },
     };
-    return this.http.get(`/manga`, this.options);
+    return this.http.get(`${BASE_URL}/manga`, this.options);
   }
 
   public getCoverFileName(id_cover_art: string): Observable<any> {
-    return this.http.get(`/cover/${id_cover_art}`);
+    return this.http.get(`${BASE_URL}/cover/${id_cover_art}`);
   }
 
   public getMangaByTitle(title: string): Observable<any> {
     const options = { params: new HttpParams().set('title', title) };
-    return this.http.get(`/manga/`, options);
+    return this.http.get(`${BASE_URL}/manga/`, options);
   }
 
   public getMangaCover(id_cover_art: string, file_name: string): string {
-    return `/covers/${id_cover_art}/${file_name}.256.jpg`;
+    return `${BASE_IMAGE_URL}/covers/${id_cover_art}/${file_name}.256.jpg`;
   }
 
   public getMangaChapterList(
@@ -65,7 +64,7 @@ export class GetMangaService {
       };
     }
 
-    return this.http.get(`/chapter?manga=${id_manga}`, this.options);
+    return this.http.get(`${BASE_URL}/chapter?manga=${id_manga}`, this.options);
   }
 
   public getMangaByVolume(
@@ -94,7 +93,7 @@ export class GetMangaService {
       };
     }
 
-    return this.http.get(`/chapter?manga=${id_manga}`, this.options);
+    return this.http.get(`${BASE_URL}/chapter?manga=${id_manga}`, this.options);
   }
 
   public getAllChapter(id_manga: string, language: string): Observable<any> {
@@ -103,15 +102,18 @@ export class GetMangaService {
         'translatedLanguage[]': language,
       },
     };
-    return this.http.get(`/manga/${id_manga}/aggregate`, this.options);
+    return this.http.get(
+      `${BASE_URL}/manga/${id_manga}/aggregate`,
+      this.options
+    );
   }
 
   public getChapterImageData(id_chapter: string): Observable<any> {
-    return this.http.get(`/at-home/server/${id_chapter}`);
+    return this.http.get(`${BASE_URL}/at-home/server/${id_chapter}`);
   }
 
   public getChapterImage(hash: string, image_chapter_data: string): string {
-    return `/data/${hash}/${image_chapter_data}`;
+    return `${BASE_IMAGE_URL}/data/${hash}/${image_chapter_data}`;
   }
 
   public getCoverId(mangaItem: { relationships: Array<any> }): string {
