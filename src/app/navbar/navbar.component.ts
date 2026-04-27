430579d7bd0c8bb2a81abfe7362165fe53cf640d
liara987 <987.liara@gmail.com>
2026-04-27 17:54:52 -0300

    fix: corrige carrosel

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,35 +1,44 @@
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { IconDirective } from '@coreui/icons-angular';
-import { cilHome } from '@coreui/icons';
-import { GetMangaService } from '../services/getManga.service';
-import { FormModule, ListGroupModule } from '@coreui/angular';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormModule, ListGroupModule } from '@coreui/angular';
+import { cilHome } from '@coreui/icons';
+import { IconDirective } from '@coreui/icons-angular';
+import { GetMangaService } from '../services/getManga.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, IconDirective, FormModule, FormsModule, ListGroupModule],
+  imports: [
+    RouterModule,
+    IconDirective,
+    FormModule,
+    FormsModule,
+    ListGroupModule,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-  @ViewChild('list') list!: ElementRef
+  @ViewChild('list') list!: ElementRef;
   icons = { cilHome };
 
-  showResults = false
+  showResults = false;
   mangaTitle: any;
-  title: string = ''
-  listResult = [{ title: '', image: '' }]
+  title: string = '';
+  listResult = [{ title: '', image: '' }];
   allData: any;
 
   constructor(
     private renderer: Renderer2,
     private _router: Router,
-    private mangaService: GetMangaService) {
+    private mangaService: GetMangaService,
+  ) {}
+
+  ngAfterViewInit() {
     this.renderer.listen('window', 'click', (e: Event) => {
-      if (!this.list.nativeElement.contains(e.target)) {
-        this.listResult = []
+      if (this.list && !this.list.nativeElement.contains(e.target)) {
+        this.listResult = [];
       }
     });
   }
@@ -38,43 +47,50 @@ export class NavbarComponent {
     this.mangaService.getMangaByTitle(title).subscribe({
       next: (mangaData: any) => {
         if (mangaData) {
-          this.showResults = true
+          this.showResults = true;
         }
-        this.allData = mangaData.data
+        this.allData = mangaData.data;
       },
       error: (error) => {
         console.error(error);
       },
       complete: () => {
         this.allData.forEach((mangaItem: any) => {
-          this.title = mangaItem.attributes.title.en
-          const coverId = this.mangaService.getCoverId(mangaItem)
+          this.title = mangaItem.attributes.title.en;
+          const coverId = this.mangaService.getCoverId(mangaItem);
 
-          this.mangaService.getCoverFileName(coverId).subscribe((cover: any) => {
-            const fileName = cover.data.attributes.fileName
-            const mangaID = cover.data.relationships.find(({ type }: any) => type === 'manga').id
-            this.listResult.push({ title: mangaItem.attributes.title.en, image: this.mangaService.getMangaCover(mangaID, fileName) })
-          });
+          this.mangaService
+            .getCoverFileName(coverId)
+            .subscribe((cover: any) => {
+              const fileName = cover.data.attributes.fileName;
+              const mangaID = cover.data.relationships.find(
+                ({ type }: any) => type === 'manga',
+              ).id;
+              this.listResult.push({
+                title: mangaItem.attributes.title.en,
+                image: this.mangaService.getMangaCover(mangaID, fileName),
+              });
+            });
         });
-      }
-    })
+      },
+    });
   }
 
   onChapterClick(title: string, image: string) {
     this._router.navigateByUrl(`/`, { skipLocationChange: true }).then(() => {
-      this._router.navigate(['/manga', title, image])
-    })
+      this._router.navigate(['/manga', title, image]);
+    });
 
-    this.listResult = []
-    this.mangaTitle = ''
+    this.listResult = [];
+    this.mangaTitle = '';
   }
 
   valueChange(event: any) {
-    this.listResult = []
-    this.searchManga(event)
+    this.listResult = [];
+    this.searchManga(event);
   }
 
   hideResults() {
-    this.listResult = []
+    this.listResult = [];
   }
 }
