7da68f4c0ff16c45fe0a4b35188c1a51714b5b35
liara987 <987.liara@gmail.com>
2026-05-03 19:04:20 -0300

    refact: melhora interface e caregamento do read chapter

--- src/app/read-chapter/read-chapter.component.ts
+++ src/app/read-chapter/read-chapter.component.ts
@@ -1,121 +1,117 @@
-import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, signal, HostListener } from '@angular/core';
+import { Location, CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { GetMangaService } from '../services/getManga.service';
-import { GoToTopComponent } from "../go-to-top/go-to-top.component";
-import { DropdownModule, ButtonModule } from '@coreui/angular';
-import { ActivatedRoute } from '@angular/router';
-import { RouterModule, RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
-
-import { IconDirective } from '@coreui/icons-angular';
-import { cilChevronRight, cilChevronLeft } from '@coreui/icons';
-
+import { GoToTopComponent } from '../go-to-top/go-to-top.component';
+import { I18nService } from '../services/i18n.service';
 
 @Component({
   selector: 'app-read-chapter',
   standalone: true,
+  imports: [CommonModule, RouterModule, GoToTopComponent],
   templateUrl: './read-chapter.component.html',
   styleUrl: './read-chapter.component.scss',
-  imports: [
-    GoToTopComponent,
-    DropdownModule,
-    RouterLink,
-    RouterModule,
-    ButtonModule,
-    IconDirective,
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ReadChapterComponent {
-  imageList = [];
-  hash!: string;
-  images: string[] = [];
-  volumes: any[] = []
-  volumeChapter: any[] = []
-  chapterList: any[] = []
-  id_chapter = ''
-  id_manga = ''
-  lang = ''
-  chapterNumber = 1
-  icons = { cilChevronRight, cilChevronLeft };
+export class ReadChapterComponent implements OnInit {
+  images = signal<string[]>([]);
+  chapterList = signal<any[]>([]);
+  id_chapter = '';
+  id_manga = '';
+  lang = '';
+  chapterNumber: number = 1;
+  focusMode = signal(false);
+  showChapterList = signal(false);
+  loading = signal(true);
 
   constructor(
-    public _location: Location,
-    private _router: Router,
+    public location: Location,
+    private router: Router,
     private route: ActivatedRoute,
-    private mangaService: GetMangaService) { }
+    private mangaService: GetMangaService,
+    public i18nService: I18nService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  ngOnInit() {
-    this.route.params.subscribe((param) => {
-      this.id_chapter = param['id_chapter']
-      this.id_manga = param['id_manga']
-      this.lang = param['language']
-      this.chapterNumber = param['chapter_number']
-    })
+  ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      this.id_chapter = param['id_chapter'];
+      this.id_manga = param['id_manga'];
+      this.lang = param['language'];
+      this.chapterNumber = +param['chapter_number'];
+      this.loadImages();
+      this.loadChapterList();
+    });
+  }
 
-    this.mangaService.getAllChapter(this.id_manga, this.lang).subscribe({
-      next: (allChapters) => {
-        Object.keys(allChapters.volumes).forEach((volume) => {
-          this.volumeChapter.push(Object.values((allChapters.volumes[volume].chapters)))
-        })
-      },
-      error: (error) => {
-        console.error(error)
-      },
-      complete: () => {
-        this.volumeChapter.forEach(chapters => {
-          chapters.forEach((chapter: any) => {
-            this.chapterList.push(chapter)
-          });
-        })
-      }
-    })
+  t(key: string): string { return this.i18nService.t(key); }
 
+  loadImages(): void {
+    this.loading.set(true);
+    this.images.set([]);
     this.mangaService.getChapterImageData(this.id_chapter).subscribe({
-      next: (mangaImageData: any) => {
-        this.imageList = mangaImageData.chapter.data
-        this.hash = mangaImageData.chapter.hash
-      },
-      error: (error) => {
-        console.error(error)
+      next: (data: any) => {
+        const imgs = data.chapter.data.map((img: string) =>
+          this.mangaService.getChapterImage(data.chapter.hash, img)
+        );
+        this.images.set(imgs);
+        this.loading.set(false);
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  loadChapterList(): void {
+    const volumeChapters: any[][] = [];
+    this.mangaService.getAllChapter(this.id_manga, this.lang).subscribe({
+      next: (data: any) => {
+        Object.keys(data.volumes).forEach(vol => {
+          volumeChapters.push(Object.values(data.volumes[vol].chapters));
+        });
       },
       complete: () => {
-        if (this.imageList) {
-          this.imageList.forEach((x, i) => {
-            this.images[i] = this.mangaService.getChapterImage(this.hash, x)
-          });
-        }
+        const flat: any[] = [];
+        volumeChapters.forEach(chapters => chapters.forEach((c: any) => flat.push(c)));
+        this.chapterList.set(flat);
+        this.cdr.markForCheck();
       }
-    })
+    });
   }
 
-  onChapterClick(chapterId: string, other: string, chapterNumber: number) {
-    const id = other.length > 0 ? other[0] : chapterId
-    const url = `${id}/${this.id_manga}/${chapterNumber}/${this.lang}`
-
-    this._router.navigateByUrl(`/refreshChapter/${url}`, { skipLocationChange: true }).then(() => {
-      this._router.navigate([`/chapter/${url}`])
-    })
+  navigateTo(chapterId: string, others: string[], chapterNumber: number): void {
+    const id = others?.length > 0 ? others[0] : chapterId;
+    const url = `${id}/${this.id_manga}/${chapterNumber}/${this.lang}`;
+    this.router.navigateByUrl(`/refreshChapter/${url}`, { skipLocationChange: true })
+      .then(() => this.router.navigate([`/chapter/${url}`]));
+    window.scrollTo(0, 0);
+    this.showChapterList.set(false);
   }
 
-  prev() {
-    this.chapterNumber--
-    this.changeChapterByNumber()
+  prev(): void {
+    const next = this.chapterNumber - 1;
+    const ch = this.chapterList().find(c => +c.chapter === next);
+    if (ch) this.navigateTo(ch.id, ch.others || [], next);
   }
 
-  next() {
-    this.chapterNumber++
-    this.changeChapterByNumber()
+  next(): void {
+    const next = this.chapterNumber + 1;
+    const ch = this.chapterList().find(c => +c.chapter === next);
+    if (ch) this.navigateTo(ch.id, ch.others || [], next);
   }
 
-  changeChapterByNumber() {
-    const chapter = this.chapterList.filter((item) => {
-      return item.chapter === this.chapterNumber.toLocaleString()
-    })
-    this.onChapterClick(chapter[0].id, chapter[0].others, this.chapterNumber)
+  hasPrev(): boolean { return this.chapterList().some(c => +c.chapter === this.chapterNumber - 1); }
+  hasNext(): boolean { return this.chapterList().some(c => +c.chapter === this.chapterNumber + 1); }
+
+  toggleFocus(): void { this.focusMode.update(v => !v); }
+  toggleChapterList(): void { this.showChapterList.update(v => !v); }
+
+  @HostListener('document:keydown', ['$event'])
+  onKey(event: KeyboardEvent): void {
+    if (event.key === 'ArrowLeft') this.prev();
+    if (event.key === 'ArrowRight') this.next();
+    if (event.key === 'f' || event.key === 'F') this.toggleFocus();
+    if (event.key === 'Escape') { this.focusMode.set(false); this.showChapterList.set(false); }
   }
 
-  ngOnDestroy() {
-    this.mangaService.getAllChapter(this.id_manga, this.lang).subscribe()
-    this.mangaService.getChapterImageData(this.id_chapter).subscribe()
-  }
+  trackByIndex(index: number): number { return index; }
 }
