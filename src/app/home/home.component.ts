bc58438b6a0ec19adae18fb3f3f062909e17d058
liara987 <987.liara@gmail.com>
2026-05-03 19:02:51 -0300

    feat: implementa novas funcionalidade e otimiza carregamento

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,92 +1,184 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SpinnerModule } from '@coreui/angular';
-import { cilChevronCircleDownAlt } from '@coreui/icons';
-import { IconDirective } from '@coreui/icons-angular';
 import { CardComponent, typeCard } from '../card/card.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { GoToTopComponent } from '../go-to-top/go-to-top.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { GetMangaService } from '../services/getManga.service';
+import { I18nService } from '../services/i18n.service';
+
+const GENRES = [
+  'Action',
+  'Adventure',
+  'Comedy',
+  'Drama',
+  'Fantasy',
+  'Horror',
+  'Mystery',
+  'Romance',
+  'Sci-Fi',
+  'Slice of Life',
+  'Sports',
+  'Thriller',
+  'Psychological',
+  'Supernatural',
+  'Isekai',
+];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
   imports: [
-    IconDirective,
+    CommonModule,
+    RouterModule,
     NavbarComponent,
     CarouselComponent,
     CardComponent,
-    CommonModule,
-    RouterModule,
-    SpinnerModule,
     GoToTopComponent,
   ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
-  cardContent: Array<typeCard> = [];
-  test: any;
-  fileName!: string;
-  coverId!: string;
-  image!: string;
-  mangaID!: string;
-  showMangaList = false;
-  page = 1;
-  icons = { cilChevronCircleDownAlt };
+export class HomeComponent implements OnInit {
+  cardContent = signal<typeCard[]>([]);
+  popularMangas = signal<typeCard[]>([]);
+  recentMangas = signal<typeCard[]>([]);
+  loading = signal(false);
+  loadingSection = signal(true);
+  page = 0;
+  allGenres = GENRES;
+  selectedGenres: string[] = [];
+  showFilters = false;
 
-  constructor(private mangaService: GetMangaService) {}
+  constructor(
+    private mangaService: GetMangaService,
+    public i18nService: I18nService,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
+    this.loadSections();
     this.getManga();
   }
 
-  getManga() {
-    this.mangaService.getAllMangas(this.page).subscribe({
-      next: (mangaData: any) => {
-        mangaData.data.forEach((mangaItem: any) => {
-          if (mangaData.data.length < 96) {
-            this.page = mangaData.data.length;
-          }
+  t(key: string): string {
+    return this.i18nService.t(key);
+  }
 
-          // ID do cover_art para buscar o filename
-          this.coverId = this.mangaService.getCoverId(mangaItem);
-
+  loadSections(): void {
+    // Popular
+    this.mangaService.getPopularMangas().subscribe({
+      next: (data: any) => {
+        const items: typeCard[] = [];
+        data.data.forEach((mangaItem: any) => {
+          const coverId = this.mangaService.getCoverId(mangaItem);
           this.mangaService
-            .getCoverFileName(this.coverId)
+            .getCoverFileName(coverId)
             .subscribe((cover: any) => {
-              this.fileName = cover.data.attributes.fileName;
-
-              // Usa o id do próprio mangaItem (não do cover) para montar a URL da imagem
-              this.setCardContent(
-                mangaItem.id,
-                this.mangaService.getMangaCover(mangaItem.id, this.fileName),
-                mangaItem.attributes.title.en ?? Object.values(mangaItem.attributes.title)[0]
-              );
+              items.push({
+                id: mangaItem.id,
+                image: this.mangaService.getMangaCover(
+                  mangaItem.id,
+                  cover.data.attributes.fileName,
+                ),
+                title: this.mangaService.getMangaTitle(mangaItem),
+              });
+              this.popularMangas.set([...items]);
+              this.cdr.markForCheck();
             });
         });
       },
-      error: (error) => {
-        console.error(error);
+    });
+
+    // Recent
+    this.mangaService.getRecentMangas().subscribe({
+      next: (data: any) => {
+        const items: typeCard[] = [];
+        data.data.forEach((mangaItem: any) => {
+          const coverId = this.mangaService.getCoverId(mangaItem);
+          this.mangaService
+            .getCoverFileName(coverId)
+            .subscribe((cover: any) => {
+              items.push({
+                id: mangaItem.id,
+                image: this.mangaService.getMangaCover(
+                  mangaItem.id,
+                  cover.data.attributes.fileName,
+                ),
+                title: this.mangaService.getMangaTitle(mangaItem),
+              });
+              this.recentMangas.set([...items]);
+              this.cdr.markForCheck();
+            });
+        });
       },
       complete: () => {
-        this.showMangaList = true;
+        this.loadingSection.set(false);
+        this.cdr.markForCheck();
       },
     });
   }
 
-  showMore() {
-    this.page += 96;
-    this.getManga();
+  getManga(append = false): void {
+    this.loading.set(true);
+    this.mangaService.getAllMangas(this.page).subscribe({
+      next: (mangaData: any) => {
+        const newItems: typeCard[] = [];
+        mangaData.data.forEach((mangaItem: any) => {
+          const coverId = this.mangaService.getCoverId(mangaItem);
+          this.mangaService
+            .getCoverFileName(coverId)
+            .subscribe((cover: any) => {
+              newItems.push({
+                id: mangaItem.id,
+                image: this.mangaService.getMangaCover(
+                  mangaItem.id,
+                  cover.data.attributes.fileName,
+                ),
+                title: this.mangaService.getMangaTitle(mangaItem),
+              });
+              this.cardContent.set(
+                append ? [...this.cardContent(), ...newItems] : [...newItems],
+              );
+              this.cdr.markForCheck();
+            });
+        });
+      },
+      complete: () => {
+        this.loading.set(false);
+        this.cdr.markForCheck();
+      },
+    });
   }
 
-  setCardContent(id: string, image: string, title: string) {
-    this.cardContent.push({
-      id: id,
-      image: image,
-      title: title,
-    });
+  showMore(): void {
+    this.page += 28;
+    this.getManga(true);
+  }
+
+  toggleGenre(genre: string): void {
+    const idx = this.selectedGenres.indexOf(genre);
+    if (idx >= 0) this.selectedGenres.splice(idx, 1);
+    else this.selectedGenres.push(genre);
+  }
+
+  isSelectedGenre(genre: string): boolean {
+    return this.selectedGenres.includes(genre);
+  }
+
+  clearGenres(): void {
+    this.selectedGenres = [];
+  }
+
+  trackById(_: number, item: typeCard): string {
+    return item.id;
   }
 }
