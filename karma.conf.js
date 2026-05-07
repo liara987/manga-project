809434d961b32645b8eb7f0af6a27567885ba98d
liara987 <987.liara@gmail.com>
2026-05-07 01:32:58 -0300

    fix: corrige karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,46 @@
-const path = require('path');
+const path = require("path");
 
 if (!process.env.CHROME_BIN) {
   try {
-    process.env.CHROME_BIN = require('puppeteer').executablePath();
-  } catch {
-    // Falls back to karma-chrome-launcher's default Chrome lookup.
-  }
+    process.env.CHROME_BIN = require("puppeteer").executablePath();
+  } catch {}
 }
-
-const localChromeLibPath = path.join(
-  __dirname,
-  '.cache/chrome-libs/root/usr/lib/x86_64-linux-gnu',
-);
-
-process.env.LD_LIBRARY_PATH = [
-  localChromeLibPath,
-  process.env.LD_LIBRARY_PATH,
-]
-  .filter(Boolean)
-  .join(':');
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
+
     client: {
       jasmine: {},
       clearContext: false,
     },
-    jasmineHtmlReporter: {
-      suppressAll: true,
-    },
+
     coverageReporter: {
-      dir: path.join(__dirname, './coverage/manga-project'),
-      subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      dir: path.join(__dirname, "./coverage/manga-project"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
-    reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadlessNoSandbox'],
+
+    reporters: ["progress"],
+
+    browsers: ["ChromeHeadlessNoSandbox"],
+
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
       },
     },
-    restartOnFileChange: true,
+
+    singleRun: true,
+    restartOnFileChange: false,
   });
 };
