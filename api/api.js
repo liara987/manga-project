6c7f8dbcf166ab3619ef44bdc598c61f8f4e6f11
liara987 <987.liara@gmail.com>
2026-06-18 20:26:29 -0300

    volta pro codigo original

--- api/api.js
+++ api/api.js
@@ -3,10 +3,6 @@ const https = require("https");
 module.exports = async (req, res) => {
   const targetPath = req.url.replace(/^\/api/, ""); // → /manga?limit=96&offset=1&...
   const fullTarget = `https://api.mangadex.org${targetPath}`; // query string já inclusa
-  const data = await response.text();
-  console.log("Status:", response.status);
-  console.log("CF-Ray:", response.headers.get("cf-ray"));
-  console.log("Body preview:", data.slice(0, 200)); // ver se é HTML do Cloudflare
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
@@ -21,9 +17,8 @@ module.exports = async (req, res) => {
     const response = await fetch(fullTarget, {
       headers: {
         Referer: "https://mangadex.org",
-        "User-Agent": "manga-project/1.0 (987.liara@gmail.com)",
-        Accept: "application/json",
-        "Accept-Language": "en-US,en;q=0.9",
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Encoding": "identity",
       },
     });
 
