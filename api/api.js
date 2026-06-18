66dfed04fb873872a8fcb6f054358604e35bde8d
liara987 <987.liara@gmail.com>
2026-06-18 16:57:26 -0300

    adiciona agent no header

--- api/api.js
+++ api/api.js
@@ -3,6 +3,10 @@ const https = require("https");
 module.exports = async (req, res) => {
   const targetPath = req.url.replace(/^\/api/, ""); // → /manga?limit=96&offset=1&...
   const fullTarget = `https://api.mangadex.org${targetPath}`; // query string já inclusa
+  const data = await response.text();
+  console.log("Status:", response.status);
+  console.log("CF-Ray:", response.headers.get("cf-ray"));
+  console.log("Body preview:", data.slice(0, 200)); // ver se é HTML do Cloudflare
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
@@ -17,8 +21,7 @@ module.exports = async (req, res) => {
     const response = await fetch(fullTarget, {
       headers: {
         Referer: "https://mangadex.org",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/138.0.0.0 Safari/537.36",
+        "User-Agent": "manga-project/1.0 (987.liara@gmail.com)",
         Accept: "application/json",
         "Accept-Language": "en-US,en;q=0.9",
       },
